30f52c901127c6fce7ec165c035db8c45fe6da2a
olik09 <mhoam2@gmail.com>
2020-06-12 07:02:46 +0300

    - Temporary LOGO - Sign in button - Sign up button - New Activity when Sign in is pressed - New activity contains email and password inputs - Log in button (no action yet)

--- app/src/main/java/com/example/realestatebroker/MainActivity.java
+++ app/src/main/java/com/example/realestatebroker/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.realestatebroker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.realestatebroker.ui.login.LoginActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        System.out.println("Hello, this is just a test");
-        System.out.println("Try 2");
+        button = (Button) findViewById(R.id.signin);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Login();
+            }
+        });
+    }
+
+    public void Login() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
